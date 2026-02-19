a054f0bf2eae64c5e63de920bc5a1cfc7d43f3c4
poom-56 <37215@bpk.ac.th>
2026-02-19 20:31:14 +0700

    Add initial variable declarations and console outputs
    This script declares several variables and logs their values and combinations to the console in multiple sections.

---
+++ work4-inhour.js
@@ -0,0 +1,38 @@
+// ประกาศตัวแปรตามที่โจทย์กำหนด
+let n1 = 100;
+let n2 = 2.5;
+let a = "A";
+let b = "B";
+let c5 = "C5";
+let h = "Hello";
+
+// การแสดงผลข้อที่ 1
+console.log(n1);
+console.log(n2);
+console.log(a);
+console.log(b);
+console.log(c5);
+
+console.log("-------------------");
+
+// การแสดงผลข้อที่ 2
+console.log(n1 + " " + n2);
+console.log(a + " " + b);
+console.log(c5);
+
+console.log("-------------------");
+
+// การแสดงผลข้อที่ 3
+console.log(n1 + "" + n2 + "  " + a + b + "  " + c5);
+
+console.log("-------------------");
+
+// การแสดงผลข้อที่ 4
+console.log(n1 + h);
+console.log(n2 + h);
+console.log(h + a);
+
+console.log("-------------------");
+
+// การแสดงผลข้อที่ 5
+console.log(n1 + h + n2 + h + a);
